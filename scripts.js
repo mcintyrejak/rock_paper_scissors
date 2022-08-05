99e8b3fa619905fd583fbd35605fdcca8ec44bf3
Jamie McIntyre <mcintyrejak@gmail.com>
2022-08-05 11:54:34 -0400

    Create funciton playRound

--- scripts.js
+++ scripts.js
@@ -13,3 +13,24 @@ function getComputerChoice() {
 
 
 
+function playRound(playerSelection, computerChoice) {
+    if (playerSelection === computerChoice) {
+        return "It's a tie!"
+    } else if (playerSelection === "Rock" && computerChoice === "Scissors") {
+        return "You win!"
+    } else if (playerSelection === "Scissors" && computerChoice === "Rock") {
+        return "You win!"
+    } else if (playerSelection === "Paper" && computerChoice === "Rock") {
+        return "You win!"
+    } else {
+        return "Computer wins, bitch!"
+    }
+
+}
+const playerSelection = "Rock";
+console.log("Player choice: " + playerSelection);
+const computerChoice = getComputerChoice();
+console.log("Computer choice: " + computerChoice);
+console.log(playRound(playerSelection, computerChoice));
+
+
