f18f5556f8e723c2efbef866ce89b27e60d85328
Jamie McIntyre <mcintyrejak@gmail.com>
2022-08-06 11:56:45 -0400

    Get a random choice from the computer, get a choice from the player.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,43 @@
-getComputerChoice(){
+function getComputerChoice() {
     // Get the computer's choice of rock, paper, scissors
-    // Output computer's choice
+    // Output the computer's choice
+    let ranNum = Math.floor((Math.random() * 3) + 1)
+    if (ranNum === 1) {
+        return "Rock"
+    } else if (ranNum === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
 
-checkInputs(){
-    // Check that case doesn't matter
-}
+console.log("Computer choice: ", getComputerChoice());
 
-getPlayerChoice(){
+// checkInputs(){
+//     // Check that case doesn't matter
+// }
+
+function getPlayerChoice() {
     // Get the player's choice of rock, paper, scissors
     // Output the player's choice
+    let playerChoice = prompt("Choose rock, paper, or scissors");
+    console.log("Player choice: ", playerChoice)
 }
 
-determineWinner(){
-    // Compare computer's choice to player's choice
-    // Output winner
-}
+console.log(getPlayerChoice())
 
-game(){
-    // Play five rounds
-    // Display winners
-}
 
-overallWinner(){
-    // Determine the winner of all five rounds
-}
+// determineWinner(){
+//     // Compare computer's choice to player's choice
+//     // Output winner
+// }
+
+// game(){
+//     // Play five rounds
+//     // Display winners
+// }
+
+// overallWinner(){
+//     // Determine the winner of all five rounds
+// }
 
