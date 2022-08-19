59e6f31190cf9fcf7a494291c80bbdab4c884f49
Jamie McIntyre <mcintyrejak@gmail.com>
2022-08-19 11:25:59 -0400

    create function that gets random computer choice, create function that plays one round

--- scripts.js
+++ scripts.js
@@ -1,53 +1,32 @@
-// global variables //
+// global variables
 const choices = ["rock", "paper", "scissors"]
+const playerSelection = "rock"
+const computerSelection = getComputerChoice()
+let playerWins = 0
+let computerWins = 0
+let ties = 0
 
-function game() {
-    // play the game
-    // play five rounds
-    // console based
-    playRound();
+// gets a random computer choice of rock, paper, scissors
+function getComputerChoice() {
+    let ranNum = Math.floor(Math.random() * 3);
+    return choices[ranNum]
 }
 
-function playRound() {
-    // save the random computer selection in a variable for use 
-    // save the player inputted choice in a variable for use
-    const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
-}
-
-function playerChoice() {
-    // get input from the player
-    let input = prompt("Rock, paper, or scissors?");
-    // don't let the user cancel
-    while (input === null) {
-        input = prompt("You need to choose rock, paper, or scissors")
+// plays one round of rock, paper, scissors
+function playRound(playerChoice, computerChoice) {
+    // computerChoice = computerSelection;
+    // playerChoice = playerSelection;
+    if (playerChoice === computerChoice) {
+        ties++;
+        return "Tie!"
+    } else if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "scissors" && computerChoice === "paper" || playerChoice === "paper" && computerChoice === "rock") {
+        playerWins++;
+        return "Player wins!"
+    } else {
+        computerWins++;
+        return "Computer wins!"
     }
-    // make the input case insensitive
-    input = input.toLowerCase();
-
-    // validate the user's input matches an item in the choices array 
-    // prompt the user if it doesn't
-
-    let check = validateInput(input);
-    while (check === false) {
-        input = prompt("You need to choose rock, paper, or scissors");
-        while (input === null) {
-            input = prompt("You need to choose rock, paper, or scissors")
-        }
-        input = input.toLowerCase();
-        check = validateInput(input); // keeps prompting user until check returns true
-    }
-
 }
 
-function computerChoice() {
-    // this function returns a random choice of rock, paper, scissors, from the choices array //
-    return choices[Math.floor(Math.random() * choices.length)]
-}
+console.log(playRound(playerSelection, computerSelection))
 
-function validateInput(choice) {
-    // make sure what the user types matches an item in the choices array (rock, paper, or scissors)
-    choices.includes(choice) // will return true if choices includes choice
-}
-
-game();
